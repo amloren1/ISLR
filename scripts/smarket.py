2b4228b17ea897f67c22758c8644f2b920824f80
amloren1 <alexlorenzo39@gmail.com>
2019-08-23 16:26:05 -0700

    use statsmodels logit model

--- scripts/smarket.py
+++ scripts/smarket.py
@@ -10,22 +10,53 @@ else:
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
 sns.set(style="ticks", color_codes=True)
-from sklearn.linear_model import LogisticRegression
+import scipy
+#from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+import statsmodels.api as sm
 
 class Data(object):
     def __init__(self):
 
         # pull in data, drop "day" column for now
         self.raw_data = pd.read_csv(fp + "Smarket.csv").drop("day", axis=1)
-        self.raw_data.replace({"Direction": {"Up": 1, "Down": -1}}, inplace=True)
-        self.raw_features = self.raw_data.loc[:, self.raw_data.columns != "Direction"]
+        self.raw_data.replace({"Direction": {"Up": 1, "Down": 0}}, inplace=True)
+        #self.raw_features = self.raw_data.loc[:, self.raw_data.columns != "Direction"]
+        self.raw_features = self.raw_data.drop(["Direction", "Today", "Year"], axis=1)
+        self.raw_y = self.raw_data.loc[:, self.raw_data.columns == "Direction"]
 
-        breakpoint()
 
+
+
+
+def get_rss(y_train, y_pred):
+    """"
+        residual sum of squares for the prediction
+    """
+    rss = 0
+    for i in range(len(y_train)):
+        rss += (y_test[i][0] - y_pred[i][0]) ** 2
+
+    return (rss / (len(y_test) - 2)) ** 0.5
+
+def get_slopes_se(self, X, y_pred):
+    rss = self.get_rss(y_pred)
+    x_mean = np.mean(X)
+    x_var = list(map(lambda x: (x - x_mean) ** 2, X))
+    x_var = sum(x_var)
+
+    return (rss ** 2 / x_var) ** 0.5
+
+def coef_summary(X, y_pred, coef):
+    b1_std_err = self.get_slopes_se(X, y_pred)
+
+    t_statistic = coef / b1_std_err
+
+    p_val = scipy.stats.norm.sf(abs(t_statistic))  # one-sided
+    print(p_val)
+    breakpoint()
+    return b1_std_err, t_statistic, p_val
 
 def sec_4_6_1(data):
     """
@@ -45,14 +76,24 @@ def sec_4_6_2(data):
         logistic regression model to predict Direction or stock market on any given day based
         on lag1-5 and volume (previous day's)
     """
-    logmodel = LogisticRegression()
+    # breakpoint()
 
     x_train, x_test, y_train, y_test = train_test_split(
-        digits.data, digits.target, test_size=0.25, random_state=0
+        data.raw_features["Volume", "Lag1"], data.raw_y, test_size=0.01, random_state=0
     )
+    logit = sm.Logit(data.raw_y,data.raw_features)
+    # fit the model
+    result = logit.fit()
+    print(result.summary())
+    conf_matrix = result.pred_table(0.5)
 
+
+    print("           down pred   up pred")
+    print(f"down true {conf_matrix[0,0]}       {conf_matrix[0,1]}  ")
+    print(f"up true   {conf_matrix[1,0]}        {conf_matrix[1,1]}")
 
 if __name__ == "__main__":
     data = Data()
 
-    sec_4_6_1(data)
+    #sec_4_6_1(data)
+    sec_4_6_2(data)
